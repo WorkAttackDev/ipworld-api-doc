c3c12873f954a7a725fffe2841028c0c8cb9c08b
WorkAttackDev <workattackgeral@gmail.com>
2021-09-24 07:22:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-1gqiq7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-1gqiq7)
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+//@ts-ignore
+import Alpine from 'alpinejs';
+
+import './style.css';
+
+//@ts-ignore
+window.Alpine = Alpine;
+
+Alpine.start();
